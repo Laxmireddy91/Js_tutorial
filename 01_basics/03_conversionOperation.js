7656a3e93c262b5032fa1bc35520a480660df87a
Laxmireddy91 <laxmireddysobba91@gmail.com>
2026-09-04 14:56:24 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,51 @@
+let score =33
+
+console.log(typeof score);
+
+
+let age="23";
+console.log(typeof (age));
+
+let valueInNumber = Number(age)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+//nan
+let marks="50abc"
+console.log(typeof (marks))
+let valueInNumber2 = Number(marks)
+console.log(typeof (valueInNumber2))
+console.log(valueInNumber2)
+
+//null
+let score2 = null
+console.log(typeof (score2))
+let valueInNumber3 = Number(score2)
+console.log(typeof (valueInNumber3))
+console.log(valueInNumber3)
+
+
+
+//undifine
+let score3 = undefined
+console.log(typeof (score3))
+let valueInNumber4 = Number(score3)
+console.log(typeof (valueInNumber4))
+console.log(valueInNumber4)
+
+//boolean
+let score4 = true
+console.log(typeof (score4))
+let valueInNumber5 = Number(score4)
+console.log(typeof (valueInNumber5))
+console.log(valueInNumber5)
+
+let isLoggedIn = "Hitesh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+//1=> true;
+// 0=> false
+//""=> false
+
