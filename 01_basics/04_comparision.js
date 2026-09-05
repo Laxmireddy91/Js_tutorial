2c59e2c725cb0e8b65249d3fd7de92740d8049ea
Laxmireddy91 <laxmireddysobba91@gmail.com>
2026-09-05 13:59:22 +0000

    memory

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -14,3 +14,24 @@ console.log(undefined<0)
 
 //===
 console.log("2"===2)
+
+//memory
+
+//stack(primitive) , heap(non-primitive)
+
+let myYoutubename = "hiteshchoudarydotcom"
+
+let anothername = myYoutubename
+anothername = "caiaurcode"
+
+console.log(myYoutubename)
+console.log(anothername);
+
+let userOne = {
+    email:"user@gmail.com",
+    upi:"user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
